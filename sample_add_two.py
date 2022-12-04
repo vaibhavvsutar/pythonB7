261b9b809a88d61f401e7dfa8c0bda5f01049efc
Vaibhav <vvsutar21185@gmail.com>
2022-12-04 16:02:00 +0530

    modified code as per review comments

--- sample_add_two.py
+++ sample_add_two.py
@@ -1,8 +1,12 @@
+# Take any two integer numbers
 a = 450
 b = 207
 add_ans = a+b
 sub_ans = a-b
+# addition
 print(f"addition is {add_ans}")
+#subtraction
 print(f"substraction is {sub_ans}")
+# table of 5
 for i in range(1,11):
     print(5*i)
